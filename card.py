1f2765348060e7549aa1d080724058f81ac0d627
Shiro0002 <148281199+Shiro0002@users.noreply.github.com>
2023-12-07 16:23:59 -0500

    Add files via upload

--- card.py
+++ card.py
@@ -1,48 +1,117 @@
+from colorama import Fore, Back, Style, init
 import random
-class Card():
 
+# Initialize colorama for colored console output
+init(autoreset=True)
+
+def print_colored(text, color=Fore.WHITE, background_color=Back.BLACK, style=Style.NORMAL):
+    """Print colored text to the console."""
+    formatted_text = f"{style}{color}{background_color}{text}"
+    print(formatted_text)
+
+class Card():
     VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     SUITS = ["Clubs", "Spades", "Diamonds", "Hearts"]
 
-    # Constructor
+    # Constructor for Card class
     def __init__(self, value, suit):
+        """Initialize a card with a given value and suit."""
         self.value = value
         self.suit = suit
 
-    # New Shuffled Deck Method
-    def new_deck(self):
-        deck = []
-        for suit in Card.SUITS:
-            for value in Card.VALUES:
-                deck.append(Card(value, suit))
+    # Class method to create a new shuffled deck
+    @classmethod
+    def new_deck(cls):
+        """Create and shuffle a new deck of cards."""
+        deck = [cls(value, suit) for suit in cls.SUITS for value in cls.VALUES]
         random.shuffle(deck)
         return deck
 
-    # Print a card as a string
+    # String representation of a card
     def __repr__(self):
-        # ♣ ♦ ♥ ♠
-        if self.suit == "Clubs":
-            symbol = "♣"
-        elif self.suit == "Spades":
-            symbol = "♠"
-        elif self.suit == "Diamonds":
-            symbol = "♦"
-        elif self.suit == "Hearts":
-            symbol = "♥"
-        else:
-            raise Exception("Invalid Suit")
+        """Return a string representation of the card."""
+        symbols = {"Clubs": "♣", "Spades": "♠", "Diamonds": "♦", "Hearts": "♥"}
+        symbol = symbols.get(self.suit, None)
+        if symbol is None:
+            raise ValueError("Invalid Suit")
 
-        if self.value == 11: 
-            val = "J"
-        elif self.value == 12:
-            val = "Q"
-        elif self.value == 13: 
-            val = "K"
-        elif self.value == 1: 
-            val = "A"
-        else: val = self.value
+        val_map = {11: "J", 12: "Q", 13: "K", 1: "A"}
+        val = val_map.get(self.value, str(self.value))
 
         return f"{symbol}{val}"
-        
-   
-  
+
+# Calculate the value of a hand considering possible adjustments for aces
+def hand_value(hand):
+    """Calculate the value of a hand, adjusting for aces if necessary."""
+    total = sum(card.value for card in hand)
+    num_aces = sum(1 for card in hand if card.value == 1)
+    while total > 21 and num_aces:
+        total -= 10
+        num_aces -= 1
+    return total
+
+# Print the cards in a hand
+def print_hand(hand):
+    """Print the cards in a given hand."""
+    print(" ".join(str(card) for card in hand))
+
+# Blackjack game implementation
+def black_jack():
+    """Play a game of Blackjack."""
+    while True:
+        deck = Card.new_deck()
+        player_hand = [deck.pop(), deck.pop()]
+        dealer_hand = [deck.pop(), deck.pop()]
+
+        # Check for Blackjack
+        if hand_value(player_hand) == 21:
+            print("Blackjack! You won!")
+        elif hand_value(dealer_hand) == 21:
+            print("Dealer has Blackjack. You lost.")
+        else:
+            while True:
+                print(Fore.BLUE + "Your hand:")
+                print_hand(player_hand)
+
+                # Human player's move
+                add_card = input(Fore.GREEN + "Do you want to add a card? (yes or no): ")
+                if add_card.lower() == "yes":
+                    player_hand.append(deck.pop())
+                    if hand_value(player_hand) > 21:
+                        print("Bust! You lost.")
+                        break
+                else:
+                    break
+
+            # Dealer's move
+            while hand_value(dealer_hand) < 17:
+                dealer_hand.append(deck.pop())
+
+            player_score = hand_value(player_hand)
+            dealer_score = hand_value(dealer_hand)
+
+            print("\nYour hand:")
+            print_hand(player_hand)
+            print("Your score:", player_score)
+
+            print("\nDealer's hand:")
+            print_hand(dealer_hand)
+            print("Dealer's score:", dealer_score)
+
+            # Determine the winner
+            if dealer_score > 21:
+                print("Dealer bust! You won.")
+            elif player_score > dealer_score:
+                print("You won!")
+            elif player_score < dealer_score:
+                print("You lost.")
+            else:
+                print("Tie.")
+
+        play_again = input(Fore.RED + "Do you want to play again? (yes or no): ")
+        if play_again.lower() != "yes":
+            print("Thanks for playing! Goodbye.")
+            break
+
+# Start the Blackjack game
+black_jack()
